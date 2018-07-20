e49ca0c11ed066cf8c166b6187b3e2c246ae9c09
张作伟 <694065721@qq.com>
2018-07-20 13:08:26 +0800

    Initial Commit

---
+++ FaceDetection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FaceDetection
+//
+//  Created by mac on 2018/7/20.
+//  Copyright © 2018年 zhangzuowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
